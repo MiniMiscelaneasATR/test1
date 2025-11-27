6530ec5182852cdfae88f1b7dc9940121e631df1
MiniMiscelaneasATR <alejandra.tobar@gmail.com>
2025-11-27 19:53:13 -0300

    Refactor name input and add vertical printing
    Added functions to print name vertically and with spacing.

--- Test- 1.py
+++ Test- 1.py
@@ -4,3 +4,23 @@ print(nombre,largo)
 i=largo
 for i in range(largo,-1):
     print(nombre[i])
+
+def espacio(n):
+    return " " *3*n
+Nombre = input("Escriba su nombre: ")
+largo = len(Nombre)
+for i in range(largo):
+    if i == 0:
+        print(Nombre[0])
+    else:
+        print(espacio(i) + Nombre[i])
+
+def nombre_vertical():
+    Nombre = input( "Escriba su nombre: ")
+    largo = len(Nombre)
+    for i in range(0, largo):
+      print(Nombre[i] + "\n") 
+for n in range(3):
+    nombre_vertical()
+
+
